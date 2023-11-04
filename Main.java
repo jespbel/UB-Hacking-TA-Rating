8fa1d0d8afe20b9b03ec4c0cee2e0fed4df03e9d
sammyibrahim <sammyibr@buffalo.edu>
2023-11-04 14:02:06 -0400

    0.0.2 - Added comment below Wonwoo's to see if Sam can push

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         // An idiot admires comlexity, and genius admires simplicity.
+        //Daddy Yeager
+        //-Daddy Yeager
         System.out.println("Hello world!");
         // I am vengeance
     }
