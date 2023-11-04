a5991cc856462731902a87c01e42d6b821d4f6d5
jespbel <jmbelaso@buffalo.edu>
2023-11-04 19:14:36 -0400

    "0.0.6: Created Instance Variables for TA class"

--- TA.java
+++ TA.java
@@ -1,4 +1,24 @@
-public class TA {
-    private String Name;
+
+
+public class TA{
+    private String name;
+
+    private int totalRatings;
+
+    private double kindness;
+
+    private double punctuality;
+
+    private double passion;
+
+    private double comedian;
+
+    private double amazingProcter;
+
+    private double knowledge;
+
+    private double piazzaGod;
+
+    private double ohDweller;
 
 }
