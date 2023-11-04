ce42eb0bd15b7e8096d74e32300989e649745b75
elif14 <warrenww14@gmail.com>
2023-11-04 13:53:46 -0400

    0.0.1: Added one comment to see if "Wonwoo" can commit and push and other can pull the changes.

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        // An idiot admires comlexity, and genius admires simplicity.
         System.out.println("Hello world!");
     }
 }
