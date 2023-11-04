83be25a3c28ad8d87a1ecefc377cc6d00af9fc69
sammyibrahim <sammyibr@buffalo.edu>
2023-11-04 17:29:15 -0400

    0.0.5 - Course Class created with getters/setters and constructor

--- Reviewer.java
+++ Reviewer.java
@@ -1,3 +1,27 @@
+import java.util.ArrayList;
 public class Reviewer {
     private String reviewerID;
+
+    private ArrayList<TA> reviewed;
+
+    public Reviewer(String reviewerID, ArrayList<TA> reviewed) {
+        this.reviewerID = reviewerID;
+        this.reviewed = reviewed;
+    }
+
+    public String getReviewerID() {
+        return reviewerID;
+    }
+
+    public void setReviewerID(String reviewerID) {
+        this.reviewerID = reviewerID;
+    }
+
+    public ArrayList<TA> getReviewed() {
+        return reviewed;
+    }
+
+    public void setReviewed(ArrayList<TA> reviewed) {
+        this.reviewed = reviewed;
+    }
 }
