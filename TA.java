8024394937b6beb78924b66a71fa46230f5c1b9e
jespbel <jmbelaso@buffalo.edu>
2023-11-04 20:15:36 -0400

    "0.0.8: Created computed average and deleted some instance variables"

--- TA.java
+++ TA.java
@@ -1,4 +1,6 @@
-
+import java.lang.reflect.Array;
+import java.util.*;
+import java.io.*;
 
 public class TA{
     private String name;
@@ -13,12 +15,14 @@ public class TA{
 
     private double comedian;
 
-    private double amazingProcter;
-
     private double knowledge;
 
-    private double piazzaGod;
-
-    private double ohDweller;
-
+    public void computeAverage(ArrayList<Integer> user_input){
+        int numberRatings = this.totalRatings + 1;
+        this.kindness = (this.kindness/numberRatings) + (user_input.get(0)/numberRatings);
+        this.punctuality = (this.punctuality/numberRatings) + (user_input.get(1)/numberRatings);
+        this.passion = (this.passion/numberRatings) + (user_input.get(2)/numberRatings);
+        this.comedian = (this.comedian/numberRatings) + (user_input.get(3)/numberRatings);
+        this.knowledge = (this.knowledge/numberRatings) + (user_input.get(4)/numberRatings);
+    }
 }
